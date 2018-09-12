b2ff771dfa5816c875be38079481e24cd25989fb
Anjum Zaki <34029745+Anjumzaki@users.noreply.github.com>
2018-09-12 10:20:03 +0500

    Add files via upload

---
+++ client/assets/index.js
@@ -0,0 +1,22 @@
+// Icons
+import './icons/favicon.ico';
+
+// Images
+import './images/default-profile.png';
+import './images/olx-logo.jpg';
+import './images/location.png';
+import './images/search.png';
+import './images/bikes.png';
+import './images/books.png';
+import './images/cars.png';
+import './images/Electronic.png';
+import './images/fashion.png';
+import './images/furniture.png';
+import './images/jobs.png';
+import './images/kids.png';
+import './images/mobiles.jpeg';
+import './images/pets.png';
+import './images/properties.png';
+import './images/services.png';
+import './images/succes.jpg';
+import './images/allads.png';
